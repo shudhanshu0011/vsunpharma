ccbeb33bb68785965fd9c55ccc65b9bec3c3eba8
Shudhanshu Ranjan <shudhanshu0011@gmai.com>
2022-07-19 10:25:05 +0530

    upgrade

--- public/index.js
+++ public/index.js
@@ -173,3 +173,21 @@ const draw = () => {
 };
 
 draw();
+
+let imgIndex = 0;
+rollImages();
+function rollImages() {
+  var i;
+  const images = document.querySelectorAll(".slider img");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  imgIndex++;
+  if (imgIndex > images.length) {
+    imgIndex = 1;
+  }
+
+  images[imgIndex - 1].style.display = "block";
+
+  setTimeout(rollImages, 3000);
+}
